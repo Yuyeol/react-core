258723f1bfd018e4621cffc00f833faf3692b65b
Yuyeol <uy23@naver.com>
2025-01-21 17:22:19 +0900

    refac: 타이핑 정리
    - 타입 정의 중복코드 제거
    - Intrinsic: 엘리먼트에서 key값을 사용할 수 있게하는 컴파일러에서 정한 네이밍으로 변경

--- src/types/jsx.d.ts
+++ src/types/jsx.d.ts
@@ -1,18 +1,25 @@
 // JSX 작성 시 필요한 타입
-// 임시로 사용할 최소한의 속성만 정의
 declare namespace JSX {
-  interface JSXAttributes {
-    key?: string | number;
-  }
+  type CommonHTMLAttributes = HTMLAttributes<HTMLElement>;
+  type ButtonHTMLAttributes = ButtonHTMLAttributes<HTMLButtonElement>;
+  type InputHTMLAttributes = InputHTMLAttributes<HTMLInputElement>;
+
+  // IntrinsicElements & IntrinsicAttributes: 컴파일러가 인식하는 네이밍이 정해진 인터페이스
+  // 내장 요소(div, span, h1 등 기본 요소들)
   interface IntrinsicElements {
-    header: HTMLAttributes<HTMLElement> & JSXAttributes;
-    div: HTMLAttributes<HTMLElement> & JSXAttributes;
-    span: HTMLAttributes<HTMLElement> & JSXAttributes;
-    h1: HTMLAttributes<HTMLElement> & JSXAttributes;
-    p: HTMLAttributes<HTMLElement> & JSXAttributes;
-    ul: HTMLAttributes<HTMLElement> & JSXAttributes;
-    li: HTMLAttributes<HTMLElement> & JSXAttributes;
-    button: ButtonHTMLAttributes<HTMLButtonElement> & JSXAttributes;
-    input: InputHTMLAttributes<HTMLInputElement> & JSXAttributes;
+    header: CommonHTMLAttributes;
+    div: CommonHTMLAttributes;
+    span: CommonHTMLAttributes;
+    h1: CommonHTMLAttributes;
+    p: CommonHTMLAttributes;
+    ul: CommonHTMLAttributes;
+    li: CommonHTMLAttributes;
+    button: ButtonHTMLAttributes;
+    input: InputHTMLAttributes;
+  }
+
+  // JSX 전용 속성을 엘리먼트(Intrinsic, UserDefined 모두에 해당)에서 사용 할 수 있도록 별도로 추가
+  interface IntrinsicAttributes {
+    key?: string | number;
   }
 }

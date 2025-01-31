00ce329f25849f94166072e6b562c7a6a0ce4435
Yuyeol <uy23@naver.com>
2025-01-31 16:34:15 +0900

    refac: useEffect 리팩토링
    - 불필요한 분기처리 제거
    - any타입 보강

--- src/utils/core/hooks/effect.ts
+++ src/utils/core/hooks/effect.ts
@@ -3,41 +3,39 @@ import { updateSchedule } from "@/utils/core/hooks/scheduler";
 
 type TCleanupFunction = () => void;
 type TEffectCallback = () => void | TCleanupFunction;
-type TDependencyList = any[] | null;
+// unknown read only
+type TDependencyList = readonly unknown[];
 
 export interface IEffect {
   callback: TEffectCallback;
-  deps: TDependencyList;
+  deps?: TDependencyList;
   cleanup?: TCleanupFunction;
 }
 
-const shouldRerunEffect = (
-  prevDeps: TDependencyList,
-  nextDeps: TDependencyList
+const shouldRerun = (
+  prevDeps: TDependencyList | undefined,
+  nextDeps: TDependencyList | undefined
 ): boolean => {
   // deps 파라미터를 아예 전달하지 않은 경우 (매 렌더링마다 실행)
-  if (prevDeps === null) return true;
-  if (nextDeps === null) return true;
+  if (prevDeps === undefined) return true;
+  if (nextDeps === undefined) return true;
   // deps가 빈 배열인 경우 (마운트 시에만 실행)
   if (Array.isArray(nextDeps) && nextDeps.length === 0) return false;
   // deps 값 비교
   return prevDeps.some((dep, i) => !Object.is(dep, nextDeps[i]));
 };
 
-const isInitialRun = (currentIndex: number) => {
-  return global.getEffectAt(currentIndex) === undefined;
-};
-
-export const useEffect = (
-  callback: TEffectCallback,
-  deps: TDependencyList = null
-) => {
+export const useEffect = (callback: TEffectCallback, deps: TDependencyList) => {
   const currentIndex = global.getEffectIndex();
+  const prevEffect = global.getEffectAt(currentIndex);
+  const shouldRun = !prevEffect || shouldRerun(prevEffect.deps, deps);
 
-  if (isInitialRun(currentIndex)) {
-    console.log("initial run");
+  if (shouldRun) {
+    // 이펙트 실행전 이전 클린업이 있다면 먼저 실행
+    if (prevEffect?.cleanup) prevEffect.cleanup();
     updateSchedule({
       effect: () => {
+        // 이펙트 실행과 동시에 클린업 수집
         const cleanup = callback() || undefined;
         global.setEffectAt(currentIndex, {
           callback,
@@ -46,27 +44,8 @@ export const useEffect = (
         });
       },
     });
-  } else {
-    const prevEffect = global.getEffectAt(currentIndex);
-    const prevDeps = prevEffect?.deps;
-
-    if (shouldRerunEffect(prevDeps, deps)) {
-      updateSchedule({
-        effect: () => {
-          // 이펙트 실행전 이전 클린업이 있다면 먼저 실행
-          if (prevEffect.cleanup) {
-            prevEffect.cleanup();
-          }
-          // 이펙트 실행과 동시에 클린업 수집
-          const cleanup = callback() || undefined;
-          global.setEffectAt(currentIndex, {
-            callback,
-            deps,
-            cleanup,
-          });
-        },
-      });
-    }
   }
+
+  // shouldRun과 관계없이 인덱스 재부여를 위해 실행
   global.incrementEffectIndex();
 };
